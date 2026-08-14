ed396b1709e17e91ba240894664ebda6ce0a2dc4
iantso <iantsochristianrazafindrazaka@gmail.com>
2026-08-14 12:53:19 +0100

    cleaning boilerplate code

--- script/teleport_lv_1.gd
+++ script/teleport_lv_1.gd
@@ -5,7 +5,5 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		var mainscene=get_tree().current_scene # Replace with function body.
-		#mainscene.switch_lvl(1,"spawn")
+		var mainscene=get_tree().current_scene 
 		mainscene.call_deferred("switch_lvl",1,"spawn")
-		print("method has been called")
